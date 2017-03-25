4f30b4afbc252754977a35426479b7484c076f08
Denys Panov <denys.panov@gmail.com>
2017-03-25 18:53:35 +0200

    first

---
+++ fractal.h
@@ -0,0 +1,13 @@
+#ifndef FRACTAL_H
+# define FRACTAL_H
+# include "mlx.h"
+# include "libft/libft.h"
+# include <math.h>
+typedef struct	s_data
+{
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*data;
+};
+#endif
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include "fractal.h"
+
+int		main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+
+	}
+	else
+	{
+
+	}
 }
